22b454cbcb6cd5c7e0c5c8ac5461c2e07e99e058
promsurinm@hotmail.com <skr010527>
2017-04-23 23:44:12 +0700

    Tmp Hook

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php
+<-php
  
-$strAccessToken = "ACCESS_TOKEN";
+$strAccessToken = "QWiSqwAAs1/FyPo+Rt+jKoxjjK+LbkQ1pC1zsmCO9s5g2YO9EFUsSKO90ABQpc8h31iecVkjMsG3IZ2J9xCcS5pHL0ph8nc81PIM+gJEFzkJpHIRBWiJQl7sh6dOuuApuPMC+aj1HjkT5iaHCXDJ5AdB04t89/1O/w1cDnyilFU=";
  
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
@@ -11,26 +11,26 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "??????"){
+if($arrJson['events'][0]['message']['text'] == "------"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "?????? ID ?????? ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "????????"){
+  $arrPostData['messages'][0]['text'] = "------ ID ------ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "--------"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "?????????????????";
-}else if($arrJson['events'][0]['message']['text'] == "?????????????"){
+  $arrPostData['messages'][0]['text'] = "-----------------";
+}else if($arrJson['events'][0]['message']['text'] == "-------------"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "?????????????????? ????????????????????";
+  $arrPostData['messages'][0]['text'] = "------------------ --------------------";
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "??????????????????";
+  $arrPostData['messages'][0]['text'] = "------------------";
 }
  
  
@@ -45,4 +45,4 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
  
-?>
+->
