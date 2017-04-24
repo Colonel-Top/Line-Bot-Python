1268e7066841e9d0fd10eadf1522f85f52e399d2
root <promsurinm@hotmail.com>
2017-04-24 04:45:30 -0400

    Upfate

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == 'Hello'){
+if($arrJson['events'][0]['message']['text'] == 'Report API ID'){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
