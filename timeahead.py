023e7dcff7992362aaf6820d588c802ae097ccc4
promsurinm@hotmail.com <skr010527>
2017-04-24 22:19:48 +0700

    Add Files timeahead.py

--- timeahead.py
+++ timeahead.py
@@ -131,8 +131,9 @@ while (True):
                     #print("*")
                 if curhour >= 1:
                     tmphour = curhour-1
-                    line_bot_api.push_message(destination, TextSendMessage(text = '1'+tmphour))
-                    line_bot_api.push_message(destination, TextSendMessage(text = '2'+curhour))
+                    line_bot_api.push_message(destination, TextSendMessage(tmphour))
+                    line_bot_api.push_message(destination, TextSendMessage(curhour))
+                    line_bot_api.push_message(destination, TextSendMessage(text = "Done"))
                 if(curday == now.day and curmonth == now.month and curyear == now.year and tmphour == now.hour):
                 #if(1):
                     try:
