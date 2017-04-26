4db51254f4806b0e6d7fa30181fbbfd49bb16d8c
Colonel-Top <promsurinm@hotmail.com>
2017-04-26 19:22:32 +0700

    Add Time customer

--- python/chat.py
+++ python/chat.py
@@ -20,7 +20,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
- gdate = (now.strftime('%d-%m-%Y'))
+gdate = (now.strftime('%d-%m-%Y'))
 customerfile = '/customer/'+gdate
 line_bot_api = LineBotApi('QWiSqwAAs1/FyPo+Rt+jKoxjjK+LbkQ1pC1zsmCO9s5g2YO9EFUsSKO90ABQpc8h31iecVkjMsG3IZ2J9xCcS5pHL0ph8nc81PIM+gJEFzkJpHIRBWiJQl7sh6dOuuApuPMC+aj1HjkT5iaHCXDJ5AdB04t89/1O/w1cDnyilFU=')
 fo = open('control','r+')
